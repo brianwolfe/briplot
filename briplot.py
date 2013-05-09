d4ca1f6d51819cd744b45cbabdefa5cb47f659b8
Brian Wolfe <wolfe.br@gmail.com>
2013-05-09 12:42:54 -0700

    Added an option for row headers.
    This may not always work, but I needed it for my current paper.

--- briplot.py
+++ briplot.py
@@ -6,13 +6,13 @@ from copy import copy
 from math import ceil
 axislabels = list(string.ascii_lowercase) + [''.join((a, b)) for a in string.ascii_lowercase for b in string.ascii_lowercase]
 
-def set_square(fig, width=1.9, **kwargs):
+def set_square(fig, width=1.9, x_ax_pad=0.33, y_ax_pad=0.40, **kwargs):
     # Set to square
-    set_rectangular(width=width, aspect=1.0, **kwargs)
+    set_rectangular(fig, width=width, aspect=1.0, x_ax_pad=x_ax_pad, y_ax_pad=y_ax_pad, **kwargs)
 
 def set_rectangular(fig, width=1.75, aspect=1.6, n_columns=1,
                     padding=[0, 0, 0, 0], points=None, y_ax_pad=0.35,
-                    x_ax_pad=0.25):
+                    x_ax_pad=0.27, axis_padding=[0, 0, 0, 0], toplabels=[]):
     """
     Set all the axes inside to have width of width and an aspect ratio
     (width / height) of aspect
@@ -26,16 +26,26 @@ def set_rectangular(fig, width=1.75, aspect=1.6, n_columns=1,
 
     # Figure out the layout
     n_rows = int(ceil(float(len(ax_list)) / n_columns))
+    add_toplabels = False
+    axis_toppadding = 0
 
+    if len(toplabels) > 0 and len(toplabels) != n_rows:
+        print 'Not adding labels. Wrong length. ' +  \
+            'n_rows = {} n_labels = {}'.format(n_rows, len(toplabels))
+    elif len(toplabels) > 0:
+        add_toplabels = True
+        axis_toppadding = 0.05
+
+    height = width / aspect
     # Figure out the size we should be
     if points is None:
         points = [[plt.rcParams['figure.subplot.left'],
                     plt.rcParams['figure.subplot.bottom']],
                   [plt.rcParams['figure.subplot.right'],
-                    plt.rcParams['figure.subplot.top']]]
+                    plt.rcParams['figure.subplot.top'] - axis_toppadding]]
+
     norm_width = points[1][0] - points[0][0]
     norm_height = points[1][1] - points[0][1]
-    height = width / aspect
 
     per_ax_width = width / norm_width
     per_ax_height = height / norm_height
@@ -50,14 +60,21 @@ def set_rectangular(fig, width=1.75, aspect=1.6, n_columns=1,
     else:
         padding[1] += -(points[0][1]) - (1 - points[1][1]) * per_ax_height
 
-
     tot_width = per_ax_width * (n_columns) + padding[0] + padding[2]
     tot_height = per_ax_height * (n_rows) + padding[1] + padding[3]
 
     x_ax_pad_n = x_ax_pad / per_ax_height
     y_ax_pad_n = y_ax_pad / per_ax_width
 
+
     for i in range(n_rows):
+        if len(toplabels) == n_rows:
+            textx = 0.5
+            texty = (per_ax_height * (n_rows - i - 1) + padding[1] +
+                            points[0][1] * per_ax_height + height + axis_toppadding + 11./72.) / tot_height
+            fig.text(textx, texty, toplabels[i], fontsize='large',
+                    horizontalalignment='center', verticalalignment='baseline')
+
         for j in range(n_columns):
             cur_ax = i*n_columns + j
             if cur_ax < len(ax_list):
@@ -72,9 +89,12 @@ def set_rectangular(fig, width=1.75, aspect=1.6, n_columns=1,
                 ax_list[cur_ax].set_position(box)
                 ax_list[cur_ax].xaxis.set_label_coords(0.5, -x_ax_pad_n)
                 ax_list[cur_ax].yaxis.set_label_coords(-y_ax_pad_n, 0.5)
-                ax_list[cur_ax].text(-y_ax_pad_n*1.15, 1.05, axislabels[cur_ax], transform=ax_list[cur_ax].transAxes,
-                                    fontweight='bold', horizontalalignment='right',
-                                    fontsize='large')
+                if n_rows > 1 or n_columns > 1:
+                    ax_list[cur_ax].text(-y_ax_pad_n*1.15, 1.05,
+                            axislabels[cur_ax],
+                            transform=ax_list[cur_ax].transAxes,
+                            fontweight='bold', horizontalalignment='right',
+                            fontsize=9)
                 grids = [tick.gridline for tick in ax_list[cur_ax].xaxis.get_major_ticks()]
                 grids += [tick.gridline for tick in ax_list[cur_ax].xaxis.get_minor_ticks()]
                 grids += [tick.gridline for tick in ax_list[cur_ax].yaxis.get_major_ticks()]
@@ -116,11 +136,13 @@ def cumhist(ax, x, minx=None, maxx=None, n_bins=1000, logscale=True, normalized=
         bins = np.linspace(minx - r * 0.02, maxx + 0.02, n_bins)
     hist, bins = np.histogram(x, bins)
     n_samples = len(x)
+    n_samples_below = sum(x < bins[0])
 
     if normalized:
         hist = hist / float(n_samples)
+        n_samples_below = n_samples_below / float(n_samples)
 
-    cumulative = np.cumsum(hist)
+    cumulative = np.cumsum(hist) + n_samples_below
 
     h = ax.plot(bins[:-1], cumulative, **kwargs)
     ax.set_xlim(minx, maxx)
